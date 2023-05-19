e1f6d5924bd837cef1f0d783aa7038d23f4795ee
Evgenii <ev.fogo@gmail.com>
2023-05-19 12:34:04 +0300

    Added Chest opening flow

---
+++ TechArt_Test/Assets/Scripts/Chest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class Chest : MonoBehaviour
+{
+    [SerializeField] Button button;   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(FirstOpen);
+    }
+
+    public void FirstOpen()
+    {
+        Animator firstOpenAnim = GetComponent<Animator>();
+        firstOpenAnim.Play("chest_first_open");
+        button.onClick.AddListener(OpenChest);
+        
+    }
+
+    public void OpenChest()
+    {
+        Animator openAnim = GetComponent<Animator>();
+        openAnim.Play("chest_open");
+    }
+
+    public void DisableInteraction()
+    {
+        button.interactable = false;
+    }
+
+    public void EnableInteraction()
+    {
+        button.interactable = true;
+    }
+
+}
